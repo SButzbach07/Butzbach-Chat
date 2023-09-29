5cb9d9341616fe6cea4607878b11a636eff5b64a
SButzbach07 <3361527-SButzbach07@users.noreply.replit.com>
2023-09-29 16:58:19 +0000

    Initial commit

---
+++ chat/client.js
@@ -0,0 +1,262 @@
+/*
+	Butzbach Chat
+	A web communication application using Socket.IO
+	Client-side handling (client.js)
+
+	Copyright (C) 2022 Scot Butzbach
+	Licensed under the MIT License
+	See LICENSE for terms
+*/
+
+const socket = io();
+
+const chat = document.getElementById("chat");
+const chatFilterLabel = document.getElementById("chatFilterLabel");
+const chatModal = document.getElementById("chatModal");
+const chatRoom = document.getElementById("chatRoom");
+const connected = document.getElementById("connected");
+const disableAudio = document.getElementById("disableAudio");
+const roomCodeLabel = document.getElementById("roomCodeLabel");
+const roomNameLabel = document.getElementById("roomNameLabel");
+const settingsModal = document.getElementById("settingsModal");
+const userCounter = document.getElementById("userCounter");
+const userName = document.getElementById("userName");
+const userNameLabel = document.getElementById("userNameLabel");
+const usersInRoom = document.getElementById("usersInRoom");
+let isConnected = false;
+let announcementInterval;
+
+function send() {
+	const messageInput = document.getElementById("message");
+	if (messageInput.value.length != 0) {
+		socket.emit("send", messageInput.value);
+		messageInput.value = "";
+	}
+}
+
+function addMessage(message) {
+	const messageP = document.createElement("p");
+	messageP.style.overflowWrap = "break-word";
+	messageP.textContent = message;
+	chat.appendChild(messageP);
+	chat.scrollTop = chat.scrollHeight;
+}
+
+function addUser(user) {
+	const userP = document.createElement("p");
+	userP.style.overflowWrap = "break-word";
+	userP.textContent = user;
+	usersInRoom.appendChild(userP);
+	usersInRoom.scrollTop = usersInRoom.scrollHeight;
+}
+
+function clearChat() {
+	while (chat.childElementCount > 0) {
+		chat.removeChild(chat.lastElementChild);
+	}
+}
+
+function clearUserList() {
+	while (usersInRoom.firstChild) {
+		usersInRoom.removeChild(usersInRoom.firstChild);
+	}
+}
+
+function receive(message, frequency) {
+	addMessage(message);
+	if (disableAudio.checked == false) {
+		setTimeout(() => {
+			playSound(frequency, 50, 0.1);
+		}, 100);
+	}
+}
+
+function openModal(modal) {
+	document.getElementById(modal).style.display = "block";
+}
+
+function closeModal(modal) {
+	document.getElementById(modal).style.display = "none";
+}
+
+function createRoom(name, room) {
+	if (window.navigator.onLine == false) {
+		receive("Error: You are not online. Please reconnect to create a chat room.", 349);
+	} else {
+		if (chatModal.style.display = "block") {
+			closeModal("chatModal");
+		}
+
+		if (name.length == 0) {
+			receive("Error: User name must be entered.", 349);
+		} else if (name.length > 40) {
+			receive("Error: User name exceeded the 40 character limit.", 349);
+		} else if (/\s/g.test(name)) {
+			receive("Error: User name contains whitespace.", 349);
+		} else if (room.length == 0) {
+			receive("Error: Room name must be entered.", 349);
+		} else if (room.length > 40) {
+			receive("Error: Room name exceeded the 40 character limit.", 349);
+		} else {
+			if (isConnected) {
+				clearInterval(announcementInterval);
+				clearUserList();
+				socket.emit("leave");
+				isConnected = false;
+			}
+			
+			clearChat();
+			socket.emit("create", name, room, (room.toLowerCase().endsWith("_nofilter")) ? false : true);
+			userName.value = "";
+			chatRoom.value = "";
+		}
+	}
+}
+
+function joinRoom(name, code) {
+	if (window.navigator.onLine == false) {
+		receive("Error: You are not currently online. Please reconnect to join a chat room.", 349);
+	} else {
+		if (chatModal.style.display = "block") {
+			closeModal("chatModal");
+		}
+
+		if (name.length == 0) {
+			receive("Error: User name must be entered.", 349);
+		} else if (name.length > 40) {
+			receive("Error: User name exceeded the 40 character limit.", 349);
+		} else if (/\s/g.test(name)) {
+			receive("Error: User name contains whitespace.", 349);
+		} else if (code.length == 0) {
+			receive("Error: Room code must be entered.", 349);
+		} else if (code.length != 8) {
+			receive("Error: Room code must be 8 characters.", 349);
+		} else if (/^[a-z0-9]+$/i.test(code) == false) {
+			receive("Error: Room code contains non-alphanumeric characters.", 349);
+		} else {
+			if (isConnected) {
+				clearInterval(announcementInterval);
+				clearUserList();
+				socket.emit("leave");
+				isConnected = false;
+			}
+			
+			clearChat();
+			socket.emit("join", name, code);
+			userName.value = "";
+			chatRoom.value = "";
+		}
+	}
+}
+
+function leaveRoom() {
+	if (chatModal.style.display = "block") {
+		closeModal("chatModal");
+	}
+
+	if (isConnected == false) {
+		receive("Error: You are not currently connected to a chat room.", 349);
+	} else {
+		socket.emit("leave");
+		clearInterval(announcementInterval);
+		clearUserList();
+		isConnected = false;
+		userNameLabel.textContent = "";
+		roomNameLabel.textContent = "";
+		roomCodeLabel.textContent = "";
+		chatFilterLabel.textContent = "";
+		userCounter.textContent = "";
+		connected.textContent = "Not Connected";
+		document.title = "Not Connected - Butzbach Chat";
+	}
+}
+
+socket.on("join", (name, room, code, filter) => {
+	connected.textContent = "Connected";
+	userNameLabel.textContent = name;
+	roomNameLabel.textContent = room;
+	roomCodeLabel.textContent = code;
+	chatFilterLabel.textContent = filter;
+	isConnected = true;
+	document.title = "Connected - Butzbach Chat";
+	announcementInterval = setInterval(() => {
+		socket.emit("announcement");
+	}, 300000);
+});
+
+socket.on("receive", (type, message) => {
+	switch (type) {
+		case "announcement":
+			receive(message, 494);
+			break;
+		case "error":
+			receive(message, 349);
+			break;
+		case "message":
+			receive(message, 440);
+			break;
+		case "nonotification":
+			addMessage(message);
+			break;
+		case "sound":
+			if (disableAudio.checked == false) {
+				new Audio(message).play();
+			}
+			break;
+	}
+});
+
+socket.on("roomUpdate", (userCount, userList) => {
+	clearUserList();
+	userCounter.textContent = userCount;
+	for (let user of userList) {
+		addUser(user);
+	}
+});
+
+socket.on("disconnect", (reason) => {
+	switch (reason) {
+		case "ping timeout":
+			receive("Client: Your session has timed out due to inactive use.", 349);
+			break;
+		case "transport close":
+			receive("Client: You have been disconnected from the server.", 349);
+			break;
+		case "transport error":
+			receive("Client: An error occurred when connecting to the server.", 349);
+			break;
+		default:
+			receive(`Client: Your session has been interrupted. Reason: ${reason}`, 349);
+			break;
+	}
+
+	clearInterval(announcementInterval);
+	clearUserList();
+	isConnected = false;
+	userNameLabel.textContent = "";
+	roomNameLabel.textContent = "";
+	roomCodeLabel.textContent = "";
+	chatFilterLabel.textContent = "";
+	userCounter.textContent = "";
+	connected.textContent = "Not Connected";
+	document.title = "Not Connected - Butzbach Chat";
+});
+
+document.addEventListener("keypress", (event) => {
+	if (event.code === "Enter" || event.code === "NumpadEnter") {
+		send();
+	}
+});
+
+window.onload = () => {
+	receive("Welcome to Butzbach Chat. If you see this message, then there is a connection established with the server. To join a chat room, press the Chat Room button at the top of your screen. Create a chat room by providing a user name and a room name, or join a chat room by providing a user name and a room code. The side bar will provide information about the connected chat room.", 440);
+	document.title = "Not Connected - Butzbach Chat";
+}
+
+window.onclick = (event) => {
+	if (event.target == chatModal) {
+		closeModal("chatModal");
+	} else if (event.target == settingsModal) {
+		closeModal("settingsModal");
+	}
+}
---
+++ index.js
@@ -0,0 +1,395 @@
+/*
+	Butzbach Chat
+	A web communication application using Socket.IO
+	Server-side handling (index.js)
+
+	Copyright (C) 2022 Scot Butzbach
+	Licensed under the MIT License
+	See LICENSE for terms
+*/
+
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { clean } = require("censorjs");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+	cors: {
+		origin: ["https://scot.butzbach.net"]
+	}
+});
+
+app.use(express.static("chat", {
+	index: "app.html"
+}));
+
+const announcements = [
+	"Butzbach Chat has moved to \'scot.butzbach.net\'! You can still chat at the host site \'butzbach-chat.sbutzbach07.repl.co\' because it will always be available. For more information, please visit the project page at Scot\'s website.",
+	"Visit Scot\'s website at \'https://scot.butzbach.net\'.",
+	"If you want to see the developmental version of Butzbach Chat, go to \'https://butzbach-chat-development.sbutzbach07.repl.co\'. There is no implementation of the developmental version on Scot\'s website.",
+	"If you want to revisit the time of playing Flash games, head to Scot\'s website and go to the Games page. Yes, with help from some projects, Flash games can be playable again.",
+	"Ever wondered people play sounds in this app? Every sound has a nine letter phrase. When sent, it plays a sound to everyone, along with an announcement of who played what. The list of phrases can be found by using the /listsounds command, the looking at the app's project page on Scot's website, or looking at the code for the project on Replit.",
+	"Butzbach Chat features a command system. Use \"/help\" or \"/h\" to display all commands available to use.",
+	"Butzbach Chat is licensed under the MIT License. Visit the project page on Replit for terms.",
+	"Butzbach Chat supports room creation. Create your own rooms to chat with your friends. A room code is automatically generated when a room is created.",
+	"Butzbach Chat features a chat filter system using the \"censorjs\" package from NPM. Unfiltered chat rooms end with \"_nofilter\" Be responsible, be respectful, and stay safe online."
+];
+
+const sounds = [
+	"evictroom",
+	"olderinet",
+	"stickykey",
+	"amogusrep",
+	"vsaucehey",
+	"mccaveamb",
+	"goooofyah",
+	"sheeeeesh",
+	"amogusjob",
+	"stoooopid",
+	"emodamage",
+	"bobeatbox",
+	"ballerbmm",
+	"boomvines",
+	"hehehehaa",
+	"dasideeye",
+	"amogusmix",
+	"skibisong",
+	"branpizza",
+	"brokentab",
+	"metalpipe",
+	"cocacolae",
+	"beeperror",
+	"yellgoofy",
+	"ohhnoobro",
+	"zzzzsnore",
+	"auuuuuugh",
+	"xpearrape",
+	"cicecream",
+	"goodnight",
+	"rickrolll"
+];
+
+const users = {};
+const rooms = {};
+
+async function wait(time) {
+	await new Promise(resolve => setTimeout(resolve, time));
+}
+
+function generateRoomCode() {
+	const roomCodeCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	let roomCode = "";
+
+	for (let i = 0; i < 8; i++) {
+		roomCode += roomCodeCharacters[Math.floor(Math.random() * roomCodeCharacters.length)];
+	}
+
+	return roomCode;
+}
+
+function checkUserNameExists(userName) {
+	for (let id of Object.keys(users)) {
+		if (users[id].name == userName) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function checkRoomNameExists(roomName) {
+	for (let code of Object.keys(rooms)) {
+		if (rooms[code].name == roomName) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function getUsersFromRoom(roomCode) {
+	const usersInRoom = [];
+
+	for (let id of Object.keys(users)) {
+		if (users[id].room == roomCode) {
+			usersInRoom.push(users[id].name);
+		}
+	}
+
+	return usersInRoom;
+}
+
+io.on("connection", (socket) => {
+	socket.on("create", (name, room, filter) => {
+		if (checkRoomNameExists(room)) {
+			socket.emit("receive", "error", "Error: Chat room already exists.");
+		} else if (filter && clean(room) != room) {
+			socket.emit("receive", "error", "Error: Room name contains profane content. All chat rooms are created with the chat filter enabled.");
+		} else if (checkUserNameExists(name)) {
+			socket.emit("receive", "error", "Error: User name already exists.");
+		} else if (filter && clean(name) != name) {
+			socket.emit("receive", "error", "Error: User name contains profane content. All chat rooms are created with the chat filter enabled.");
+		} else {
+			const code = generateRoomCode();
+			rooms[code] = {
+				name: room,
+				code: code,
+				userCount: 1,
+				chatFilter: filter
+			};
+
+			users[socket.id] = {
+				name: name,
+				room: code
+			};
+
+			socket.join(code);
+			io.in(code).emit("receive", "message", `Server: Welcome to the \"${room}\" chat room, ${name}.`);
+			socket.emit("join", name, room, code, (rooms[code].chatFilter) ? "Enabled" : "Disabled");
+			io.in(code).emit("roomUpdate", rooms[code].userCount, getUsersFromRoom(code));
+		}
+	});
+
+	socket.on("join", (name, code) => {
+		if (checkUserNameExists(name)) {
+			socket.emit("receive", "error", "Error: User name already exists.");
+		} else if (rooms[code] == undefined) {
+			socket.emit("receive", "error", "Error: Chat room does not exist.");
+		} else if (rooms[code].chatFilter == true && clean(name) != name) {
+			socket.emit("receive", "error", "Error: User name contains profane content. The chat room you are trying to join has the chat filter enabled.");
+		} else {
+			users[socket.id] = {
+				name: name,
+				room: code
+			};
+
+			rooms[code].userCount += 1;
+			socket.join(code);
+			socket.emit("receive", "message", `Server: Welcome to the \"${rooms[code].name}\" chat room, ${name}.`);
+			socket.in(code).emit("receive", "message", `Server: Welcome, ${name}.`);
+			socket.emit("join", name, rooms[code].name, code, (rooms[code].chatFilter) ? "Enabled" : "Disabled");
+			io.in(code).emit("roomUpdate", rooms[code].userCount, getUsersFromRoom(code));
+		}
+	});
+
+	socket.on("send", async (message) => {
+		let messageArray = [];
+		let privateMessage = "";
+
+		if (/^\/[a-z0-9]+/i.test(message)) {
+			messageArray = message.split(" ");
+			switch (messageArray[0]) {
+				case "/h":
+				case "/help":
+					if (messageArray.length == 1) {
+						socket.emit("receive", "message", "Available commands: /h, /help, /ls, /listsounds, /pas, /playallsounds, /ps, /playsound, /pm, /privatemessage, /prs, /privatesound.");
+					} else if (messageArray.length == 2) {
+						switch (messageArray[1]) {
+							case "h":
+							case "/h":
+								socket.emit("receive", "message", "Usage: /h [command_name]. Alias of /help. Displays all available commands or the usage of a single command. \"command_name\" is the name of the command, and the parameter is optional.");
+								break;
+							case "help":
+							case "/help":
+								socket.emit("receive", "message", "Usage: /help [command_name]. Displays all available commands or the usage of a single command. \"command_name\" is the name of the command, and the parameter is optional.");
+								break;
+							case "ls":
+							case "/ls":
+								socket.emit("receive", "message", "Usage: /ls. Alias of /listsounds. Lists every sound phrase available in the chat app. There are no parameters for this command.");
+								break;
+							case "listsounds":
+							case "/listsounds":
+								socket.emit("receive", "message", "Usage: /listsounds. Lists every sound phrase available in the chat app. There are no parameters for this command.");
+								break;
+							case "pas":
+							case "/pas":
+								socket.emit("receive", "message", "Usage: /pas. Alias of /playallsounds. Plays every sound in the chat app, except for \"rickrolll\" as it is too long to play. There are no parameters for this command.");
+								break;
+							case "playallsounds":
+							case "/playallsounds":
+								socket.emit("receive", "message", "Usage: /playallsounds. Plays every sound in the chat app, except for \"rickrolll\" as it is too long to play. There are no parameters for this command.");
+								break;
+							case "ps":
+							case "/ps":
+								socket.emit("receive", "message", "Usage: /ps [sound_phrase]. Alias of /playsound. Plays a sound to everyone in the chat room. \"sound_phrase\" is the name of the sound phrase. The full list of phrases can be found on the project page on Scot\'s website, or by using /listsounds.");
+								break;
+							case "playsound":
+							case "/playsound":
+								socket.emit("receive", "message", "Usage: /playsound [sound_phrase]. Plays a sound to everyone in the chat room. \"sound_phrase\" is the name of the sound phrase. The full list of phrases can be found on the project page on Scot\'s website, or by using /listsounds.");
+								break;
+							case "pm":
+							case "/pm":
+								socket.emit("receive", "message", "usage: /pm [recipient] [message]. Alias of /privatemessage. Sends a message to a specific user in the chat room. \"recipient\" is the name of the user in the chat room. \"message\" is the message sent to the recipient.");
+								break;
+							case "privatemessage":
+							case "/privatemessage":
+								socket.emit("receive", "message", "Usage: /privatemessage [recipient] [message]. Sends a message to a specific user in the chat room. \"recipient\" is the name of the user in the chat room. \"message\" is the message sent to the recipient.");
+								break;
+							case "prs":
+							case "/prs":
+								socket.emit("receive", "message", "Usage: /prs [recipient] [sound_phrase]. Alias of /privatesound. Plays a sound to a specific user in the chat room. \"recipient\" is the name of the user in the chat room. \"sound_phrase\" is the name of the sound phrase. The full list of phrases can be found on the project page on Scot\'s website, or by using /listsounds.");
+								break;
+							case "privatesound":
+							case "/privatesound":
+								socket.emit("receive", "message", "Usage: /privatesound [recipient] [sound_phrase]. Plays a sound to a specific user in the chat room. \"recipient\" is the name of the user in the chat room. \"sound_phrase\" is the name of the sound phrase. The full list of phrases can be found on the project page on Scot\'s website, or by using /listsounds.");
+								break;
+							default:
+								socket.emit("receive", "error", `Error: ${messageArray[1]} is not a command.`);
+						}
+					} else {
+						socket.emit("receive", "error", `Error: ${messageArray[0]} takes 0 or 1 arguments. You gave ${messageArray.length - 1}.`);
+					}
+					break;
+				case "/ls":
+				case "/listsounds":
+					if (messageArray.length == 1) {
+						socket.emit("receive", "message", `Available sound phrases: ${sounds.slice(0).sort().join(", ")}`);
+					} else {
+						socket.emit("receive", "error", `Error: ${messageArray[0]} takes 0 arguments. You gave ${messageArray.length - 1}.`);
+					}
+					break;
+				case "/pas":
+				case "/playallsounds":
+					if (messageArray.length == 1) {
+						io.in(users[socket.id].room).emit("receive", "nonotification", `Server: OH NO! ${users[socket.id].name} JUST USED THE "${messageArray[0]}" COMMAND! TURN DOWN YOUR VOLUME!`);
+						await wait(500);
+						for (let sound of sounds) {
+							if (sound == "rickrolll") {
+								continue;
+							} else {
+								io.in(users[socket.id].room).emit("receive", "sound", `https://scot.butzbach.net/projects/butzbach_chat/sounds/${sound}.mp3`);
+								await wait(Math.floor(Math.random() * 750) + 750);
+							}
+						}
+					} else {
+						socket.emit("receive", "error", `Error: ${messageArray[0]} takes 0 arguments. You gave ${messageArray.length - 1}.`);
+					}
+					break;
+				case "/ps":
+				case "/playsound":
+					if (messageArray.length == 2) {
+						for (let sound of sounds) {
+							if (messageArray[1] == sound) {
+								io.in(users[socket.id].room).emit("receive", "nonotification", `Server: ${users[socket.id].name} played "${sound}".`);
+								await wait(500);
+								io.in(users[socket.id].room).emit("receive", "sound", `https://scot.butzbach.net/projects/butzbach_chat/sounds/${sound}.mp3`);
+								return;
+							}
+						}
+
+						socket.emit("receive", "error", `Error: ${messageArray[1]} is not a sound phrase.`);
+					} else {
+						socket.emit("receive", "error", `Error: ${messageArray[0]} takes 1 arguments. You gave ${messageArray.length - 1}.`);
+					}
+					break;
+				case "/pm":
+				case "/privatemessage":
+					if (messageArray.length >= 3) {
+						for (let id of Object.keys(users)) {
+							if (messageArray[1] == users[id].name && users[id].room == users[socket.id].room) {
+								if (messageArray[1] == users[socket.id].name) {
+									socket.emit("receive", "error", "Error: You can not send yourself a private message.");
+								} else {
+									privateMessage = messageArray.slice(2).join(" ");
+									if (rooms[users[id].room].chatFilter) {
+										io.in(id).emit("receive", "message", `Private message from ${users[socket.id].name}: ${clean(privateMessage)}`);
+										socket.emit("receive", "message", `Private message to ${users[id].name}: ${clean(privateMessage)}`);
+									} else {
+										io.in(id).emit("receive", "message", `Private message from ${users[socket.id].name}: ${privateMessage}`);
+										socket.emit("receive", "message", `Private message to ${users[id].name}: ${privateMessage}`);
+									}
+								}
+								
+								return;
+							}
+						}
+
+						socket.emit("receive", "error", `Error: ${messageArray[1]} is not in this chat room.`);
+					} else {
+						socket.emit("receive", "error", `Error: ${messageArray[0]} takes at least 2 arguments. You gave ${messageArray.length - 1}.`);
+					}
+					break;
+				case "/prs":
+				case "/privatesound":
+					if (messageArray.length == 3) {
+						for (let id of Object.keys(users)) {
+							if (messageArray[1] == users[id].name && users[id].room == users[socket.id].room) {
+								if (messageArray[1] == users[socket.id].name) {
+									socket.emit("receive", "error", "Error: You can not play yourself a private sound.");
+								} else {
+									for (let sound of sounds) {
+										if (messageArray[2] == sound) {
+											io.in(id).emit("receive", "nonotification", `Private sound from ${users[socket.id].name}: \"${sound}\"`);
+											socket.emit("receive", "nonotification", `Private sound to ${users[id].name}: \"${sound}"`);
+											await wait(500);
+											io.in(id).emit("receive", "sound", `https://scot.butzbach.net/projects/butzbach_chat/sounds/${sound}.mp3`);
+											socket.emit("receive", "sound", `https://scot.butzbach.net/projects/butzbach_chat/sounds/${sound}.mp3`);
+											return;
+										}
+									}
+	
+									socket.emit("receive", "error", `Error: ${messageArray[2]} is not a sound phrase.`);
+								}
+								
+								return;
+							}
+						}
+
+						socket.emit("receive", "error", `Error: ${messageArray[1]} is not in the chat room.`);
+					} else {
+						socket.emit("receive", "error", `Error: ${messageArray[0]} takes 2 arguments. You gave ${messageArray.length - 1}.`);
+					}
+					break;
+				default:
+					socket.emit("receive", "error", `Error: ${messageArray[0]} is not a command.`);
+			}
+		} else {
+			if (rooms[users[socket.id].room].chatFilter) {
+				io.in(users[socket.id].room).emit("receive", "message", `${users[socket.id].name}: ${clean(message)}`);
+			} else {
+				io.in(users[socket.id].room).emit("receive", "message", `${users[socket.id].name}: ${message}`);
+			}
+		}
+	});
+
+	socket.on("announcement", () => {
+		socket.emit("receive", "announcement", `Announcement: ${announcements[Math.floor(Math.random() * announcements.length)]}`);
+	});
+	
+	
+	socket.on("leave", () => {
+		const code = (users[socket.id] != undefined) ? users[socket.id].room : null;
+		if (code != null) {
+			io.in(code).emit("receive", "message", `Server: Goodbye, ${users[socket.id].name}.`);
+			socket.leave(code);
+			rooms[code].userCount -= 1;
+			if (rooms[code].userCount == 0) {
+				delete rooms[code];
+				delete users[socket.id];
+			} else {
+				delete users[socket.id];
+				io.in(code).emit("roomUpdate", rooms[code].userCount, getUsersFromRoom(code));
+			}
+		}
+	});
+
+	socket.on("disconnecting", () => {
+		const code = (users[socket.id] != undefined) ? users[socket.id].room : null;
+		if (code != null) {
+			io.in(code).emit("receive", "message", `Server: Goodbye, ${users[socket.id].name}.`);
+			socket.leave(code);
+			rooms[code].userCount -= 1;
+			if (rooms[code].userCount == 0) {
+				delete rooms[code];
+				delete users[socket.id];
+			} else {
+				delete users[socket.id];
+				io.in(code).emit("roomUpdate", rooms[code].userCount, getUsersFromRoom(code));
+			}
+		}
+	});
+});
+
+httpServer.listen(80);
