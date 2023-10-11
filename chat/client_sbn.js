b1fe3f7079bf612c48e1812be71f5983b07db6d0
Scot Butzbach <70033034+SButzbach07@users.noreply.github.com>
2023-10-11 05:49:06 -0400

    Update client_sbn.js

--- chat/client_sbn.js
+++ chat/client_sbn.js
@@ -291,7 +291,7 @@ window.onload = () => {
 		window.history.replaceState({}, "", replacedURL);
 	} else {
 		receive("Welcome to Butzbach Chat. If you see this message, then there is a connection established with the server. To join a chat room, press the Chat Room button at the top of your screen. Create a chat room by providing a user name and a room name, or join a chat room by providing a user name and a room code. The side bar will provide information about the connected chat room. Use room code \"butzbach\" for the default chat room.", 440);
-		document.title = "Not Connected - Butzbach Chat";
+		document.title = "Not Connected - Chat - scot.butzbach.net";
 	}
 }
 
